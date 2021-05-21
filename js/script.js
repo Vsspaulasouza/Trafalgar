107929f4f0626b39979fa1121f43e08423d0ee07
Vinicius Soares <vsspaulasouza@gmail.com>
2021-05-20 22:41:08 -0300

    Parte da responsividade do slider

--- js/script.js
+++ js/script.js
@@ -14,8 +14,10 @@ tns({
   loop: true,
   items: 1,
   gutter: 40,
+  fixedWidth: 1200,
   slideBy: "page",
   nav: true,
+  center: true,
   autoplay: true,
   speed: 400,
   autoplayButtonOutput: false,
@@ -25,4 +27,39 @@ tns({
   prevButton: ".prev",
   nextButton: ".next",
   navContainer: "#nav",
+  responsive: {
+    300: {
+      fixedWidth: 250,
+    },
+    400: {
+      fixedWidth: 300,
+    },
+    500: {
+      fixedWidth: 400,
+    },
+    600: {
+      fixedWidth: 500,
+    },
+    700: {
+      fixedWidth: 600,
+    },
+    800: {
+      fixedWidth: 700,
+    },
+    900: {
+      fixedWidth: 800,
+    },
+    1000: {
+      fixedWidth: 900,
+    },
+    1100: {
+      fixedWidth: 1000,
+    },
+    1200: {
+      fixedWidth: 1100,
+    },
+    1300: {
+      fixedWidth: 1200,
+    },
+  },
 });
